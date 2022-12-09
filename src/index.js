85a40e9ac65c67591d358d9de13b309dc4b029a3
Kelsey Isaacson <keisaacson@github.com>
2022-12-09 11:27:57 -0600

    Print out values

--- src/index.js
+++ src/index.js
@@ -24,10 +24,12 @@ async function run() {
     .map(([k, v]) => `${k}=${v}`)
     .join("&");
 
-  core.info("Running new code");
+  const url = `/schedules/${scheduleId}/users?since=${startDate}&until=${endDate}`
+  core.info(`Url: ${url}`);
   pdClient
-    .get(`/schedules/${scheduleId}/users?since=${startDate}&until=${endDate}`)
+    .get(url)
     .then(({ resource }) => {
+      core.info(`Resource: ${resource}`)
       // `resource` should be a list of oncall entries
       if (resource.length > 0) {
         core.debug(`Oncalls found: ${JSON.stringify(resource)}`);
